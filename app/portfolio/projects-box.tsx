c4bb05698adf76df0702dee4203a45067e9852ba
James Shi <jamesshi117@gmail.com>
2023-12-09 20:09:04 -0800

    add react-markdown

--- app/portfolio/projects-box.tsx
+++ app/portfolio/projects-box.tsx
@@ -1,5 +1,6 @@
-import { ArrowIcon } from "components/icons";
-import Link from "next/link";
+import { ArrowIcon } from 'components/icons';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
 interface IndividualPortfolioProjectProps {
   project: Record<string, any>;
@@ -10,30 +11,33 @@ export function IndividualPortfolioProject({
 }: IndividualPortfolioProjectProps) {
   return (
     <>
-      <div className="my-6 border border-gray-300 dark:border-gray-600 px-4 rounded-lg hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-500 transition-all duration-500 ease-in-out">
-        <h2 className="font-bold text-xl mt-5">{project.title}</h2>
-        <p className="my-4">{project.description}</p>
-        <div className="flex items-center justify-start space-x-4">
+      <div className='my-6 border border-gray-300 dark:border-gray-600 px-4 rounded-lg bg-gradient-to-r from-purple-400/40 to-pink-500/40 hover:from-purple-400/50 hover:to-pink-500/50 transition-all duration-500 ease-in-out'>
+        <h2 className='font-bold text-base mt-5'>{project.title}</h2>
+        <ReactMarkdown
+          className='my-4 text-sm'
+          children={project.description}
+        />
+        <div className='flex items-center justify-start space-x-4'>
           {project.techLogos.map((techLogo, index) => (
             <div key={index}>{techLogo}</div>
           ))}
         </div>
-        <div className="flex space-x-4 items-center">
-          {project.repoLink == "" ? null : (
+        <div className='flex space-x-4 items-center'>
+          {project.repoLink == '' ? null : (
             <Link
-              className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all no-underline"
+              className='flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all no-underline'
               href={project.repoLink}
             >
               <ArrowIcon />
-              <p className="h-7 underline decoration-current">Repo</p>
+              <p className='h-7 underline decoration-current'>Repo</p>
             </Link>
           )}
           <Link
-            className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all no-underline"
+            className='flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all no-underline'
             href={project.demoLink}
           >
             <ArrowIcon />
-            <p className="h-7 underline decoration-current">Demo</p>
+            <p className='h-7 underline decoration-current'>Demo</p>
           </Link>
         </div>
       </div>
--- app/portfolio/projects.tsx
+++ app/portfolio/projects.tsx
@@ -17,7 +17,7 @@ import {
   TailwindIcon,
   TypescriptIcon,
   UnityIcon,
-} from "components/icons";
+} from 'components/icons';
 
 interface Project {
   title: string;
@@ -29,36 +29,38 @@ interface Project {
 
 const projects: Project[] = [
   {
-    title: "[WIP] Jiamei Overseas Website 💻🙆",
-    description:
-      "Designed and developed a new company website for an immigration consulting firm using Next.js 13 and ChakraUI. Being my biggest web-dev project to date, I challenged myself to learn a lot of new concepts such as mobile-first design, web optimization, and many of Next 13's features that I previously did not have a chance to play with",
+    title: 'Jiamei Overseas Website 💻🙆',
+    description: `
+Designed and developed a new company website for an immigration consulting firm using Next.js 13 and ChakraUI.
+
+Being my biggest web-dev project to date, I challenged myself to learn a lot of new concepts such as mobile-first design, web optimization, and many of Next 13's features that I previously did not have a chance to play with.`,
     techLogos: [<NextIcon />],
-    repoLink: "",
-    demoLink: "https://jiamei-overseas-jshiii117.vercel.app/",
+    repoLink: '',
+    demoLink: 'https://jiamei-overseas.vercel.app/',
   },
   {
-    title: "NextJS Portfolio Website 💻🙆‍♂️✨",
+    title: 'NextJS Portfolio Website 💻🙆‍♂️✨',
     description:
-      "Built a personal portfolio website using React, Next.js 13, and TailwindCSS.",
+      'Built a personal portfolio website using React, Next.js 13, and TailwindCSS.',
     techLogos: [
       <TypescriptIcon />,
       <ReactIcon />,
       <NextIcon />,
       <TailwindIcon />,
     ],
-    repoLink: "https://github.com/jshiii117/nextjs-portfolio",
-    demoLink: "https://oasismassagesaanich.ca/",
+    repoLink: 'https://github.com/jshiii117/nextjs-portfolio',
+    demoLink: 'https://oasismassagesaanich.ca/',
   },
   {
-    title: "Oasis Massage Saanich 💆‍♀️",
+    title: 'Oasis Massage Saanich 💆‍♀️',
     description:
-      "A responsive, static website made for a local business using Material UI and deployed using Firebase.",
+      'A responsive, static website made for a local business using Material UI and deployed using Firebase.',
     techLogos: [<JavascriptIcon />, <ReactIcon />, <MuiIcon />, <FigmaIcon />],
-    repoLink: "https://github.com/jshiii117/oasis-massage-saanich",
-    demoLink: "https://oasismassagesaanich.ca/",
+    repoLink: 'https://github.com/jshiii117/oasis-massage-saanich',
+    demoLink: 'https://oasismassagesaanich.ca/',
   },
   {
-    title: "DailyBoost 👟",
+    title: 'DailyBoost 👟',
     description:
       "It's like BeReal for exercising - submission for the 2023 UBC BizTech x EY Produthon. I developed the recording and upload functionality to Firebase RTD, as well as storing user information using MongoDB Cloud.",
     techLogos: [
@@ -68,11 +70,11 @@ const projects: Project[] = [
       <FirebaseIcon />,
       <MongoDbIcon />,
     ],
-    repoLink: "https://github.com/myung03/produHacks",
-    demoLink: "https://devpost.com/software/dailyboost-js5vxm",
+    repoLink: 'https://github.com/myung03/produHacks',
+    demoLink: 'https://devpost.com/software/dailyboost-js5vxm',
   },
   {
-    title: "Interview.Me 👓",
+    title: 'Interview.Me 👓',
     description:
       "Submission for the 2022 LabLabAI Open AI Hackathon. Using OpenAI GPT-3 and Whisper APIs, we built a virtual interviewer who can provide customized behavioural questions based on an applicant's specified company and role. Recorded 2,300 site visits and roughly 9,000 prompts submitted",
     techLogos: [
@@ -82,30 +84,30 @@ const projects: Project[] = [
       <MuiIcon />,
       <FlaskIcon />,
     ],
-    repoLink: "https://github.com/jshiii117/Interview.Me",
-    demoLink: "https://www.youtube.com/watch?v=snYQF07WZMo&ab_channel=JamesShi",
+    repoLink: 'https://github.com/jshiii117/Interview.Me',
+    demoLink: 'https://www.youtube.com/watch?v=snYQF07WZMo&ab_channel=JamesShi',
   },
   {
-    title: "Insighteur V1.1.0+ 📱",
+    title: 'Insighteur V1.1.0+ 📱',
     description:
-      "Overhauled the UI of the Insighteur app. I also helped ship new core functionalities of the Flutter application including a new page routing and navigation feature and a caching mechanism to cut 50% of querying cost.",
+      'Overhauled the UI of the Insighteur app. I also helped ship new core functionalities of the Flutter application including a new page routing and navigation feature and a caching mechanism to cut 50% of querying cost.',
     techLogos: [
       <DartIcon />,
       <FlutterIcon />,
       <AwsIcon />,
       <AndroidStudioIcon />,
     ],
-    repoLink: "https://apps.apple.com/ca/app/insighteur/id1614686665",
-    demoLink: "https://apps.apple.com/ca/app/insighteur/id1614686665",
+    repoLink: 'https://apps.apple.com/ca/app/insighteur/id1614686665',
+    demoLink: 'https://apps.apple.com/ca/app/insighteur/id1614686665',
   },
 
   {
-    title: "Timeline Tactics 🕹️",
+    title: 'Timeline Tactics 🕹️',
     description:
-      "My first coding project that pushed me into software dev! Timeline Tactics is a 2D, turn-based multiplayer game made with friends. I developed the battle mechanism and multiplayer functionalities built with Mirror networking library. I also designed the pixel characters using Aseprite!",
+      'My first coding project that pushed me into software dev! Timeline Tactics is a 2D, turn-based multiplayer game made with friends. I developed the battle mechanism and multiplayer functionalities built with Mirror networking library. I also designed the pixel characters using Aseprite!',
     techLogos: [<CSharpIcon />, <UnityIcon />],
-    repoLink: "https://github.com/jshiii117/timeline-tactics",
-    demoLink: "https://timelinetactics.wordpress.com/",
+    repoLink: 'https://github.com/jshiii117/timeline-tactics',
+    demoLink: 'https://timelinetactics.wordpress.com/',
   },
 ];
 
